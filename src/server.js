63392280e2c4d42b26371fbb55e7930f221d55ff
ajfero <ajfero.dev@gmail.com>
2022-10-25 12:01:22 -0300

    build: install dovenv and environments for management variables. add better comments and update the .env.example

--- src/server.js
+++ src/server.js
@@ -1,18 +1,39 @@
-// import { Express } from 'express';
-const express = require('express');
+/* Sever.js Settings Server */
 
-// Settings Definitions
-const hostname = '127.0.0.1';
-const port = 3000;
+/**
+ *  Imports and Requirements
+*/
+const express = require('express'); // Framework { Express } from 'express' for petitions Http.
+require("dotenv").config(); // For using environments variables.
+// const router = require('./config/network'); // Router -> Api Routes config
+// const router = require('./app/network/router');
 
+/**
+ * App Settings -> Definitions
+*/
 var app = express();
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 9000; // http://localhost/9000
+
+/**
+ * Middleware
+*/
+app.use(express.json());
+// router(app);
+
+/**
+ * Routes -> Prefix
+*/
+// app.use("/api", routes);
 
 // Create a route for /root
 app.use('/', function (req, res) {
     res.send('Hello, World!\n');
 });
 
-// App definitions using framework express.
+/**
+ * Create Server -> Server listening
+*/
 app.listen(port, hostname, () => {
-    console.log(`app running at http://${hostname}:${port}/`);
+    console.log(`app running at http://${hostname}:${port}/`); // http://localhost/9000
 });
