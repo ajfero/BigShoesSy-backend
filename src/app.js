aaa24750b08987cc3b0127f339dbadab6a1e7175
ajfero <ajfero.dev@gmail.com>
2022-11-15 22:20:47 -0300

    refactor: update connect to database with local environment.

--- src/app.js
+++ src/app.js
@@ -5,32 +5,32 @@ const { sequelize } = require('./database/models/index');
 const { config } = require("dotenv");
 
 /***************** Local Enviroment set to .env ******************/
-// const localhost = '127.0.0.1'; // Ip-local hostdirection.
-// const port = process.env.PORT || 3000;
+const localhost = '127.0.0.1'; // Ip-local hostdirection.
+const port = process.env.PORT || 3000;
 
-// // Starting the server
-// http.listen(PORT, () => {
-//     console.log(`App running at: http://${localhost}:${port}/`);
-//     sequelize.sync({ force: false }).then(() => {
-//         console.log('Database connection is Successfully!!');
-//     }).catch(error => {
-//         console.log('Something was wrong. error:', error);
-//     })
-// });
+// Starting the server
+http.listen(port, () => {
+    console.log(`App running at: http://${localhost}:${port}/`);
+    sequelize.sync({ force: false }).then(() => {
+        console.log('Database connection is Successfully!!');
+    }).catch(error => {
+        console.log('Something was wrong. error:', error);
+    })
+});
 
 /***************** Prod Enviroment *******************************/
-const db = mysql.createConnection({
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 3000,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD, //this is a really bad idea
-    database: process.DB_DATABASE
-});
+// const db = mysql.createConnection({
+//     host: process.env.DB_HOST || '127.0.0.1',
+//     port: process.env.DB_PORT || 3000,
+//     user: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD, //this is a really bad idea
+//     database: process.DB_DATABASE
+// });
 
-db.connect((err) => {
-    if (err) {
-        console.log(err.message);
-        return;
-    }
-    console.log("Api connect to Database AWS RDS Success!!");
-});
+// db.connect((err) => {
+//     if (err) {
+//         console.log(err.message);
+//         return;
+//     }
+//     console.log("Api connect to Database AWS RDS Success!!");
+// });
